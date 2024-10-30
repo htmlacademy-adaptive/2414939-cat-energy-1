2bc431f985d9e93adf77a891f9512696b2d109b5
Иван <orient@211.ru>
2024-10-30 20:32:14 +0700

    Автоматизация сборки личного проекта

--- gulpfile.js
+++ gulpfile.js
@@ -134,7 +134,7 @@ const reload = (done) => {
   const watcher = () => {
   gulp.watch('source/sass/*.scss', gulp.series(styles));
   gulp.watch('source/js/script.js', gulp.series(script));
-  gulp.watch('source/*.html', gulp.series(html, reload));
+  gulp.watch('source/*.html', gulp.series(html));
 
 }
 
